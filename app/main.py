8613da8728efdb0b165e0de68c52cccce2685ff9
unknown <elouardy.abderrahim07@gmail.com>
2024-07-21 20:34:17 +0100

    config: link the postgres with the code
    using the psycopg binary depandency to link the python code with
    postgresql and teste it using a get methode "dbtest" .

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+import psycopg
+
 
 app = FastAPI()
 
+@app.get("/dbtest")
+async def root():
+    with psycopg.connect("dbname=batchdemo user=postgres password=admin") as conn:
+
+#  database="batchdemo",
+#      user="postgres",
+#      password="admin",
+#      host="localhost",
+#      port="5432",
+        with conn.cursor() as cur:
+            cur.execute(
+
+            """
+                CREATE TABLE rachid (
+                id serial PRIMARY KEY,
+                num integer,
+                data text)
+            """
+        )
+
+        conn.commit()
+
+
+    return {"message": "tested db"}
 
 @app.get("/")
 async def root():
