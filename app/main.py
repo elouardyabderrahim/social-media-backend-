5d66c6a1ac02e22007d47842e5daf52755062c84
unknown <elouardy.abderrahim07@gmail.com>
2024-07-23 23:04:49 +0100

    feat: create the table post using engine

--- app/main.py
+++ app/main.py
@@ -1,33 +1,45 @@
 from fastapi import FastAPI
 import psycopg
 
+from . import models
+
+from .database import engine,get_db
 
 app = FastAPI()
 
-@app.get("/dbtest")
-async def root():
-    with psycopg.connect("dbname=batchdemo user=postgres password=admin") as conn:
 
-#  database="batchdemo",
-#      user="postgres",
-#      password="admin",
-#      host="localhost",
-#      port="5432",
-        with conn.cursor() as cur:
-            cur.execute(
+models.Base.metadata.create_all(bind=engine)
 
-            """
-                CREATE TABLE rachid (
-                id serial PRIMARY KEY,
-                num integer,
-                data text)
-            """
-        )
-
-        conn.commit()
+app = FastAPI()
 
 
-    return {"message": "tested db"}
+# get_db()
+
+# Dependency
+# @app.get("/dbtest")
+# async def root():
+#     with psycopg.connect("dbname=batchdemo user=postgres password=admin") as conn:
+
+# #  database="batchdemo",
+# #      user="postgres",
+# #      password="admin",
+# #      host="localhost",
+# #      port="5432", 
+#         with conn.cursor() as cur:
+#             cur.execute(
+
+#             """
+#                 CREATE TABLE rachid (
+#                 id serial PRIMARY KEY,
+#                 num integer,
+#                 data text)
+#             """
+#         )
+
+#         conn.commit()
+
+
+#     return {"message": "tested db"}
 
 @app.get("/")
 async def root():
