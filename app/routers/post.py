6506814813ab82430028347dc900832b42fb8ed5
unknown <elouardy.abderrahim07@gmail.com>
2024-07-29 14:37:29 +0100

    feat : get all posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter,status,Depends,Response
+from fastapi import APIRouter, HTTPException,status,Depends,Response
 
 from app.database import get_db
 from .. import schema,models
 from sqlalchemy.orm import Session
+
+from typing import List, Optional
 
 router=APIRouter(
     prefix="/posts",
     tags=["posts"]
 
 )
+
+
+@router.get("/",response_model=List[schema.Post])
+async def get_all_posts(db: Session = Depends(get_db)):
+    try:
+        posts = db.query(models.Post).all()
+        return posts
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An error occurred while fetching posts")
 
 
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schema.Post)
@@ -20,3 +31,5 @@ def create_post(post:schema.PostCreate,db: Session=Depends(get_db)):
     db.commit()
 
     return created_post
+
+
