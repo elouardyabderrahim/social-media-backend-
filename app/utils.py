4c1c0e6172cd90bfbcd24cad7b9b4278a0626291
unknown <elouardy.abderrahim07@gmail.com>
2024-08-07 15:44:21 +0100

    feat: verify password
    this change is for verifying the password that this stocked in db.

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")#deprecated="au
 # pip install bcrypt
 def hash(password: str):
     return pwd_context.hash(password)
+
+
+
+def verify_password(password:str,hashed_password:str):
+    return pwd_context.verify(password,hash=hashed_password)
